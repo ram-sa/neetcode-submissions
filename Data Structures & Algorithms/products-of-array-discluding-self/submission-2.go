a83712a373e408f4477e3c584f2e4c1a2a720a74
Ricardo Amaral <42246505+ram-sa@users.noreply.github.com>
2026-05-22 16:45:17 +0900

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.go
@@ -0,0 +1,25 @@
+func productExceptSelf(nums []int) []int {
+	//[ 1, 2, 4, 6]  Input
+	
+	//[  , 1, 2, 8]  Prefix
+	//[48,24, 6,  ]  Suffix
+	
+	//[48,24,12, 8]  Products
+
+	prds := make([]int, len(nums))
+	prds[0] = 1 // No need to initialize everything, just the first item
+    
+	prefix := 1
+    for i := 0; i < len(nums); i++ {
+        prds[i] = prefix
+        prefix *= nums[i]
+    }
+
+    postfix := 1
+    for i := len(nums) - 1; i > -1; i-- {
+        prds[i] *= postfix
+        postfix *= nums[i]
+    }
+
+    return prds
+}
